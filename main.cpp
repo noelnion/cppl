dc252f6c7c36af1bf993e2cfbb5b97b5029da786
noelnion <noelnion@gmail.com>
2023-04-15 11:55:40 +0200

    added rectangle drawing

--- main.cpp
+++ main.cpp
@@ -31,11 +31,38 @@ void print_ppm_image(std::string filename, int image_w, int image_h, std::array<
   ofs.close();
 }
 
+template<size_t w, size_t h>
+void draw_rectangle(int rect_h, int rect_w, int rect_x, int rect_y, std::array<uint32_t, w*h> &buffer, uint32_t color)
+{
+  for (int i = rect_y; i  < rect_y + rect_h; ++i)
+    {
+      for (int j = rect_y; j < rect_y + rect_w;     ++j)
+	{
+	  buffer[i*w + j] = color;
+	}
+    }
+}
+
 int main()
 {
 const  int buffer_w = 640;
 const   int buffer_h = 480;
   std::array<uint32_t, buffer_w * buffer_h>  buffer;
+
+  int map_h = 40;
+  int map_w = 40;
+  const char* map[map_h * map_w] = {"1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+    				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
+				    "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"};
 
   for (size_t i = 0; i < buffer_h; ++i)
   {
@@ -48,6 +75,8 @@ const   int buffer_h = 480;
     }
   }
 
+  draw_rectangle<buffer_w, buffer_h>(50, 50, 50, 50, buffer, pack_color(0,0,0));
+  
   std::string outname = "outppm.ppm";
   print_ppm_image<buffer_w, buffer_h>(outname, buffer_w, buffer_h, buffer);
 
