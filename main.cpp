519d02cbf65d3482536891952a82e79ff095203b
noelnion <109181000+noelnion@users.noreply.github.com>
2023-04-30 12:10:42 +0200

    hitx, hity, raymarhing loop changed to buffer_w

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <iostream>
 #include <fstream>
 #include <array>
@@ -8,14 +9,16 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
-uint32_t pack_color(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha=255)
+
+// COLORS
+uint32_t pack_color(const uint8_t &red, const uint8_t &green, const uint8_t &blue, const uint8_t &alpha=255)
 {
   uint32_t color {0};
   color = (red << 24) + (green << 16) + (blue << 8) + alpha;
   return color;
 }
 
-void unpack_color(const uint32_t color, uint8_t &red, uint8_t &green, uint8_t &blue, uint8_t &alpha)
+void unpack_color(const uint32_t &color, uint8_t &red, uint8_t &green, uint8_t &blue, uint8_t &alpha)
 {
   alpha = color & 255;
   blue =  (color >> 8) & 255;
@@ -24,7 +27,7 @@ void unpack_color(const uint32_t color, uint8_t &red, uint8_t &green, uint8_t &b
 }
 
 template<size_t w, size_t h>
-void print_ppm_image(std::string filename, int image_w, int image_h, std::array<uint32_t,(w*h)> &buffer)
+void print_ppm_image(const std::string filename, const int image_w, const int image_h, const std::array<uint32_t,(w*h)> &buffer)
 {
   std::ofstream ofs(filename);
   ofs << "P6\n" << image_w << " " << image_h <<"\n255\n";
@@ -38,7 +41,7 @@ void print_ppm_image(std::string filename, int image_w, int image_h, std::array<
 }
 
 template<size_t w, size_t h>
-void draw_pixel(int pixel_x, int pixel_y, std::array<uint32_t, w*h> &buffer, uint32_t color)
+void draw_pixel(int pixel_x, int pixel_y,  std::array<uint32_t, w*h> &buffer, uint32_t color)
 {
 	  buffer[pixel_y*w + pixel_x] = color;
 }
@@ -56,20 +59,29 @@ void draw_rectangle(int rect_h, int rect_w, int rect_x, int rect_y, std::array<u
 }
 
 template<size_t w, size_t h>
-void draw_texture(int texture_x, int texture_y, std::array<uint32_t, w*h> &buffer, std::array<uint32_t, 40*40> &texture){
-  for (int i = texture_y; i  < texture_y + 40; ++i)
+void draw_texture(const int texture_x, const int texture_y, std::array<uint32_t, w*h> &buffer, const std::array<uint32_t, 64*64> &texture){
+  for (int i = texture_y; i  < texture_y + 64; ++i)
     {
-      for (int j = texture_x; j < texture_x + 40; ++j)
+      for (int j = texture_x; j < texture_x + 64; ++j)
 	{
-	  buffer[i*w + j] = texture[(i - texture_y) * 40 + j - texture_x];
+	  buffer[i*w + j] = texture[(i - texture_y) * 64 + j - texture_x];
 	}
     }
 }
 
-std::array<uint32_t, 40*40> load_texture(std::string texture_file_name){
+template <size_t w, size_t h, size_t wall_h>
+std::array<uint32_t, wall_h> draw_texture_column (const float frac_hit, const int column_x, std::array<uint32_t, w*h> &buffer, const std::array<uint32_t, 64*64> &texture){
+
+  std::array<uint32_t, wall_h> column {0};
+  for (int y = 0; y < wall_h; 
+  
+}
+
+
+std::array<uint32_t, 64*64> load_texture(const std::string texture_file_name){
   std::fstream texture_stream(texture_file_name, std::ios_base::in);
   std::string line;
-  std::array<int, 40*40*3> texture_buffer {0};
+  std::array<int, 64*64*3> texture_buffer {0};
   int line_number = 0;
   
   while(std::getline(texture_stream, line)){
@@ -79,7 +91,6 @@ std::array<uint32_t, 40*40> load_texture(std::string texture_file_name){
     if(line_number < 4){;}
     else{
 
-      //    std::cout << line;
     int line_value = std::stoi(line);
     texture_buffer[line_number - 4] = line_value;
     }
@@ -87,14 +98,14 @@ std::array<uint32_t, 40*40> load_texture(std::string texture_file_name){
   }
 
   std::vector<uint32_t> texture_vec {0};
-  std::array<uint32_t, 40*40> texture {0};
+  std::array<uint32_t, 64*64> texture {0};
   
-  for(int i {0}; i < (40*40*3) - 3; i += 3){
+  for(int i {0}; i < (64*64*3) - 3; i += 3){
     uint8_t red   = texture_buffer[i];
     uint8_t green = texture_buffer[i+1];
     uint8_t blue  = texture_buffer[i+2];
     uint32_t color = pack_color(red, green, blue);
-    //    texture_vec.push_back(pack_color(red,green,blue));
+    //error on last pixel?
     texture[i/3] = color;
   }
 
@@ -106,151 +117,135 @@ int main()
 {
   const  int buffer_w = 640;
   const  int buffer_h = 480;
-  std::array<uint32_t, buffer_w * buffer_h>  buffer;
+  //  std::array<uint32_t, buffer_w * buffer_h>  buffer;
 
   int map_h = 40;
   int map_w = 40;
 
   int map_xsize = buffer_w/map_w;
   int map_ysize = buffer_h/map_h;
+
+  //COLORS
+  std::array<uint32_t, 5> four_colors;
+  four_colors[0] = pack_color(0,0,0);  //crna
+  four_colors[1] = pack_color(51,150,255); //modra
+  four_colors[2] = pack_color(255,92,92);  //rdeca
+  four_colors[3] = pack_color(245,235,120);  //rumena
+  four_colors[4] = pack_color(67,255,116);  //zelena
+
   
   const char* map[map_xsize * map_ysize] = {"1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1",
 					    "1", "0", "0", "0", "1", "0", "0", "0", "0", "4", "0", "0", "0", "0", "0", "1",
-					    "1", "0", "0", "0", "2", "0", "0", "0", "0", "3", "0", "0", "0", "0", "0", "1",
-					    "1", "0", "0", "0", "3", "0", "0", "0", "0", "2", "0", "0", "0", "0", "0", "1",
-					    "1", "0", "0", "0", "4", "0", "0", "0", "0", "4", "0", "0", "0", "0", "0", "1",
-					    "1", "0", "0", "0", "2", "3", "4", "2", "0", "3", "0", "0", "0", "0", "0", "1",
+					    "1", "0", "0", "0", "2", "0", "0", "0", "0", "3", "0", "0", "0", "0", "0", "2",
+					    "1", "0", "0", "0", "3", "0", "0", "0", "0", "2", "0", "0", "0", "0", "0", "3",
+					    "1", "0", "0", "0", "4", "0", "0", "0", "0", "4", "0", "0", "0", "0", "0", "4",
+					    "1", "0", "0", "0", "2", "3", "4", "2", "0", "3", "0", "0", "0", "0", "0", "3",
+					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "2",
 					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
-					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
-					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1",
-					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "2", "3", "1",
+					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "2",
+					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "2", "3", "3",
 					    "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "3", "4", "1",
 					    "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"};
 
   //draws the gradient
-  for (size_t i = 0; i < buffer_h; ++i)
-  {
-    for(size_t j = 0; j < buffer_w; ++j){
-      uint8_t r,g,b,a;
-      r = i*(255/static_cast<float>(buffer_h));
-      g = 100;
-      b = j*(255/(float)(buffer_w));
-      buffer[i*buffer_w + j] = pack_color(r,g,b);
-    }
-  }
+// for (size_t i = 0; i < buffer_h; ++i)
+// {
+//   for(size_t j = 0; j < buffer_w; ++j){
+//     uint8_t r,g,b,a;
+//     r = i*(255/static_cast<float>(buffer_h));
+//     g = 100;
+//     b = j*(255/(float)(buffer_w));
+//     buffer[i*buffer_w + j] = pack_color(r,g,b);
+//   }
+// }
 
   //draws the map
-  for (int i = 0; i < map_ysize; ++i)
-    {
-      for(int j = 0; j < map_xsize; ++j)
-	{
-	  if (map[i*map_xsize + j] != "0")
-	    {
-	      draw_rectangle<buffer_w, buffer_h>(map_h, map_w, j*map_w, i*map_h, buffer, pack_color(255,0,0));
-	    }
-	}
-    }
+//  for (int i = 0; i < map_ysize; ++i)
+//    {
+//      for(int j = 0; j < map_xsize; ++j)
+//	{
+//	  if (map[i*map_xsize + j] != "0")
+//	    {
+//	      draw_rectangle<buffer_w, buffer_h>(map_h, map_w, j*map_w, i*map_h, buffer, pack_color(255,0,0));
+//	    }
+//	}
+//    }
 
-  float player_x = 1.25;
+  float player_x = 1.1;
   float player_y = 7;
   float player_a = float(M_PI/180.f) * 0;
   float player_fov = float(M_PI/180.f) * 50;
   float d_angle = player_fov / buffer_w;
+  float player_fov2 = player_fov / 2.0;
 
-
-  //animation start
-  //  float player_a_stop = player_a + float(M_PI/180.f) * 5;
-  //  int frame_num = 0;
-  //  std::string animation_file_name = "f_";
-  /*
-    for(float animation_angle = player_a; animation_angle < player_a_stop; animation_angle += float(M_PI/180.f))
-    {
-  */  
-
-  std::array<float, buffer_w> distances {0};
-  std::array<float, buffer_w> wall_colors {0};  
+  std::array<float, buffer_w> wall_texture_index {0};  
 
   //draw player position
-  draw_rectangle<buffer_w, buffer_h>(5, 5, player_x * map_w, player_y * map_h, buffer, pack_color(0,0,0));
-
-  //main raymarching loop
-  int distance_array_index = 0;
-  for(float angle_i = player_a - (player_fov/2); angle_i < player_a + (player_fov/2); angle_i += d_angle){
-      float p = 0;
-      int wall_color;
-      for(; p < 20; p += 0.05)
-      {
-          float ray_ax = std::cos(angle_i) * p  + player_x;
-          float ray_ay = std::sin(angle_i) * p  + player_y;
-
-          draw_rectangle<buffer_w, buffer_h>(1, 1, ray_ax * map_w, ray_ay * map_h,buffer, pack_color(255,255,255));
-          if (map[(int(ray_ay) * map_xsize) + int(ray_ax)] != "0"){
-	    wall_color = std::stoi(map[(int(ray_ay) * map_xsize) + int(ray_ax)]);
-	    break;}
-      }
-
-    std::string outname = "outppm.ppm";
-  print_ppm_image<buffer_w, buffer_h>(outname, buffer_w, buffer_h, buffer);
-      
-      wall_colors[distance_array_index] = wall_color;
-      
-      //store distance and correct for fisheye
-      distances[distance_array_index] = p * std::cos(angle_i - player_a);
-      ++distance_array_index;
-  }
+  //  draw_rectangle<buffer_w, buffer_h>(5, 5, player_x * map_w, player_y * map_h, buffer, pack_color(0,0,0));
   
-
-
-  std::array<uint32_t, 5> four_colors;
-  four_colors[0] = pack_color(0,0,0);  //rumena
-  four_colors[1] = pack_color(51,150,255); //modra
-  four_colors[2] = pack_color(255,92,92);  //rdeca
-  four_colors[3] = pack_color(245,235,120);  //rumena
-  four_colors[4] = pack_color(67,255,116);  //rumena
-
-  std::string viewportname = "viewport.ppm";
-  //std::string viewportname = animation_file_name + std::to_string(frame_num);
-
   //main 3D window
+  std::string viewportname = "viewport.ppm";
   std::array<uint32_t, buffer_w * buffer_h> viewport;
 
-  //fill with brown
-  for(auto &i :  viewport)
-    {
-      i = pack_color(102, 51, 0);
-    }
+  //draw skybox and ground
+  draw_rectangle<buffer_w, buffer_h>(buffer_h/2, buffer_w, 0, 0, viewport, pack_color(30, 0, 60));
+  draw_rectangle<buffer_w, buffer_h>(buffer_h/2, buffer_w, 0, buffer_h/2, viewport, pack_color(102, 51, 0));
 
-  //draw skybox
-  draw_rectangle<buffer_w, buffer_h>(buffer_h/2, buffer_w, 0, 0, viewport, pack_color(0, 0, 0));
-
-  //calculate wall height
-  std::array<float, buffer_w> wall_height {0};
-  for(int i = 0; i < buffer_w; ++i)
-    {
-      wall_height[i] = std::min(float(buffer_h), buffer_h/distances[i]);
-    }
-
-  //draw walls
-  for(int i = 0; i < buffer_w; ++i)
-    {
-      int y_i = int(buffer_h - (buffer_h/2) - (wall_height[i]/2));
-      
-      draw_rectangle<buffer_w, buffer_h>(wall_height[i], 1, i, y_i, viewport, four_colors[wall_colors[i]]); //height, widt, x, y
-    }
-
-  auto  kr = load_texture(std::string("blue_brick_wall.ppm"));
-  draw_texture<buffer_w, buffer_h>(0, 0, viewport, kr);
   
+  //main raymarching loop
+  
+  for(int horizont = 0; horizont < buffer_w; ++horizont){
+    float angle = player_a - player_fov2 + ((player_fov/buffer_w) * horizont);
+    float p = 0;
+    float hit_x {0};
+    float hit_y {0};
+    float hit_frac {0};
+    bool is_horizontal = false;
+    int texture_index {0};
+    
+    for(; p < 20; p += 0.05)
+      {
+	float ray_ax = std::cos(angle) * p  + player_x;
+	float ray_ay = std::sin(angle) * p  + player_y;
+	int target = std::stoi(map[(int(ray_ay) * map_xsize) + int(ray_ax)]);
+	//draw_rectangle<buffer_w, buffer_h>(1, 1, ray_ax * map_w, ray_ay * map_h,buffer, pack_color(255,255,255));    //draw ray
+	if (target != 0){
+	  hit_x = ray_ax - static_cast<int>(ray_ax);
+	  hit_y = ray_ay - static_cast<int>(ray_ay);
+	  texture_index = target;
+	  break;}
+      }
+
+    if (hit_x > hit_y)
+      {
+	is_horizontal = true;
+	hit_frac = 64 * hit_x;
+      }
+    else {hit_frac = 64 * hit_y;}
+
+    
+
+    
+    //    std::string outname = "outppm.ppm";
+    //    print_ppm_image<buffer_w, buffer_h>(outname, buffer_w, buffer_h, buffer);
+      
+    wall_texture_index[horizont] = texture_index;
+    float wall_distance = p * std::cos(angle - player_a);    
+
+    //draw wall
+    float wall_height = std::min(float(buffer_h), buffer_h/wall_distance);
+    int y_i = static_cast<int>(buffer_h - (static_cast<float>(buffer_h)/2) - (wall_height/2));
+    draw_rectangle<buffer_w, buffer_h>(wall_height, 1, horizont, y_i, viewport, four_colors[wall_texture_index[horizont]]); //height, widt, x, y
+  }
+
+
+  //load and draw texture
+  auto  kr = load_texture(std::string("teal_brick_wall.ppm"));
+  draw_texture<buffer_w, buffer_h>(0, 0, viewport, kr);
+  //um
+
+  //print final 3D view
   print_ppm_image<buffer_w, buffer_h>(viewportname, buffer_w, buffer_h, viewport);
 
-  /*
-  ++frame_num;
-    }
-  //animation end
-  */
-
-
- 
-  
   return 0;
   }
